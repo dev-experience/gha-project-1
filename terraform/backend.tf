19ac751253eb3a92ec60310205c6188681825a67
Vlad DX <vlad-dx@extenza.dev>
2023-11-15 22:56:16 +0100

    Refactor terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "azurerm" {
-    subscription_id      = "8a5d8372-6c08-4017-8642-29bde3b6432e"
-    resource_group_name  = "rg-cu-ticketless-flow-terraform"
-    storage_account_name = "tklflwterraform"
-    container_name       = "tfstate-${local.environment_slug}"
-    key                  = "terraform.tfstate"
-  }
+  backend "azurerm" { }
 }
