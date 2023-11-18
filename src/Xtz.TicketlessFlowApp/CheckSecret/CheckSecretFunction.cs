c8e9a188b42c56d2291d479913174107e1aeccf0
Vlad DX <vlad-dx@extenza.dev>
2023-11-19 00:44:00 +0100

    Add `/api/check-secret` endpoint

---
+++ src/Xtz.TicketlessFlowApp/CheckSecret/CheckSecretFunction.cs
@@ -0,0 +1,35 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Xtz.TicketlessFlowApp.Core;
+
+namespace Xtz.TicketlessFlowApp.CheckSecret
+{
+    public class CheckSecretFunction
+    {
+        private readonly IOptions<CoreConnectOptions> _coreConnectOptions;
+
+        private readonly ILogger _logger;
+
+        public CheckSecretFunction(IOptions<CoreConnectOptions> coreConnectOptions, ILoggerFactory loggerFactory)
+        {
+            _coreConnectOptions = coreConnectOptions;
+            _logger = loggerFactory.CreateLogger<CheckSecretFunction>();
+        }
+
+        [Function(nameof(CheckSecretFunction))]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "check-secret")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString($"Secret is: '{_coreConnectOptions.Value.ApiKey}'");
+
+            return response;
+        }
+    }
+}
