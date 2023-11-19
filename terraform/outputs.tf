397c88d7aecb9b6604cece2fa3cc40e975f7e9b5
Vlad DX <vlad-dx@extenza.dev>
2023-11-19 20:14:21 +0100

    Fix Terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,17 @@
+data "azurerm_client_config" "current" {
+}
+
 data "azurerm_resource_group" "current" {
   name     = var.resource_group_name
 }
 
 locals {
-  resource_group_id = data.azurerm_resource_group.current.id
   tenant_id         = data.azurerm_client_config.current.tenant_id
+  resource_group_id = data.azurerm_resource_group.current.id
 }
 
 output "resource_group_portal_url" {
-  value = "https://portal.azure.com/#@${tenant_id}/resource${resource_group_id}/"
+  value = "https://portal.azure.com/#@${local.tenant_id}/resource${local.resource_group_id}/"
 }
 
 output "app_url" {
